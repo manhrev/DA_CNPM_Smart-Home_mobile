22fac72f9825ffe9f17001bddb03eae0652b10b8
manhrev <89514697+manhrev@users.noreply.github.com>
2022-03-29 09:15:01 +0700

    route complete

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import Navigator from './routes/drawer'
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import { AuthContext } from './AuthContext'
+import axios from 'axios';
+import { AsyncStorage } from 'react-native'
+
+
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch(action) {
+    case 'login':
+      return 1
+    case 'logout':
+      return 0
+    case 'adminLogin':
+      return 2
+  }
+}
+
 export default function App() {
+  // const Context = React.useContext(AuthContext)
+  // React.useEffect(() => {
+  //   checkLogin() 
+  // }, [])
+  // 
+
+  const [login, dispatch] = React.useReducer(reducer, initialState)
   return (
-    <NavigationContainer>
-      <Navigator/>
-    </NavigationContainer>
+    <AuthContext.Provider value={{loginState: login, loginDispatch: dispatch}}>
+      <Navigator />
+    </AuthContext.Provider>
+        
   );
 }
 
+// async function checkLogin() {
+//     try {
+//       const res = await axios.get('http://192.168.2.6:80/api/checkLogin')
+//       if (res.data.loggedIn) {
+//         return 1
+//       } else {
+//         return 0
+//       }
+
+//     } catch {
+//       console.log("not changed")
+//       return 1
+//     }
+
+//   }
